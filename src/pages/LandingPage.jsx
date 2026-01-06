328e8523891cdd8ae6166536a759e6c1dfa38f39
sirsulla <danielhrvth91@gmail.com>
2026-01-06 15:26:20 +0100

    add explore button to the landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -27,9 +27,10 @@ export default function LandingPage() {
     <div className = "home-container">
       <section className="section section-home">
         <img src={startImage} alt="Start logo" className="start-image" />
+        <a class="home_button" href="#description">Explore</a>
         {/*<div className="scroll-down">&#8595;</div>*/}
       </section>
-      <section className="section section-description">
+      <section id="description" className="section section-description">
         <h2>Üdvözöljük az Attila virtuális kiállítás honlapján</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         {/* ide jöhetnek a csempék */}
