740fa78f172c6f4e9080e22e083d5957dcb8c9bb
sirsulla <danielhrvth91@gmail.com>
2026-01-08 15:49:32 +0100

    fix the moving description block

--- src/pages/Tiles.jsx
+++ src/pages/Tiles.jsx
@@ -71,13 +71,13 @@ const [activeTile, setActiveTile] = useState(null);
         <CloseIcon />
       </IconButton>
 
-      <Container className="section-tiles tiles-description" sx={{position: 'sticky', top: 0, paddingBottom: 0}}>
+      <Container className="section-tiles tiles-description" sx={{paddingTop: 5, paddingBottom: 0}}>
         <h2>Üdvözöljük az Attila virtuális kiállítás honlapján</h2>
         <p> <center> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</center></p>
         <p> <center>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</center></p>
       </Container>
 
-      <Container className='tiles-theme' sx={{ py: 5 }}>
+      <Container className='tiles-theme' sx={{ py: 0, minHeight: 650}}>
         <Typography
           variant="h4"
           align="center"
